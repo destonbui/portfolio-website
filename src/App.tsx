8a2af411ef246fa1fb07d70813fc4c5031dc7987
Deston Bui <khangduy158@gmail.com>
2023-03-14 13:43:38 +0700

    Set up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { Box, Button } from "@mui/material";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  const [clicked, setClicked] = useState(false);
+
+  return (
+    <Box p={0} m={-1} minHeight="100vh" width="100vw" component="main" id="App">
+      <Box
+        minHeight="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        position="relative"
+      >
+        <motion.div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          initial={{ scale: 1 }}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <Button variant="contained">Hello</Button>
+        </motion.div>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
 );
