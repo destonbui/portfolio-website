e1b61728c0317944a8266db09d4afd507e189ac7
Deston Bui <khangduy158@gmail.com>
2023-04-06 19:10:58 +0700

    Feat: scroll to change page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState, useEffect } from "react";
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Outlet, useMatch } from "react-router-dom";
 import { Box, CssBaseline } from "@mui/material";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import Navbar from "./components/Navbar/Navbar";
 
+import { useNavigate } from "react-router-dom";
+
 function App() {
+  const isHome = Boolean(useMatch("/"));
+  const isAbout = Boolean(useMatch("/about"));
+  const isProjects = Boolean(useMatch("/projects"));
+  const isContact = Boolean(useMatch("/contact"));
+
+  const navigate = useNavigate();
+
   const darkTheme = createTheme({
     palette: {
       mode: "dark",
@@ -36,6 +45,36 @@ function App() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Box
+        onWheel={(e) => {
+          if (isHome) {
+            if (e.deltaY > 0) {
+              navigate("/about");
+            } else {
+              navigate("/");
+            }
+          }
+          if (isAbout) {
+            if (e.deltaY > 0) {
+              navigate("/projects");
+            } else {
+              navigate("/");
+            }
+          }
+          if (isProjects) {
+            if (e.deltaY > 0) {
+              navigate("/contact");
+            } else {
+              navigate("/about");
+            }
+          }
+          if (isContact) {
+            if (e.deltaY > 0) {
+              navigate("/");
+            } else {
+              navigate("/projects");
+            }
+          }
+        }}
         sx={{
           height: "100svh",
           width: "100vw",
